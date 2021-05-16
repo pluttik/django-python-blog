0ef169e698a3fb04b033062d84947273133f1ed9
Pieternella Luttikhuizen <pieternella.luttikhuizen@hva.nl>
2021-05-16 15:19:40 +0200

    moves static files

--- smtd/settings.py
+++ smtd/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-)vhr5lbbqoju^5$zbgrt71l2z^yc-dl7l$h76cji2ys^(9&b-@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["showmethedata.pythonanywhere.com"]
+ALLOWED_HOSTS = ["showmethedata.pythonanywhere.com", "127.0.0.1"]
 
 
 # Application definition
@@ -119,6 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_URL = '/media/'
 
 # Default primary key field type
