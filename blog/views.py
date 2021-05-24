5cbfae7f02b75d713b0bf4e953dd65ef09d08ea2
Pieternella Luttikhuizen <pieternella.luttikhuizen@hva.nl>
2021-05-24 21:31:10 +0200

    adds tags

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,13 @@
 from django.views.generic import ListView
-from .models import Post
+from .models import Post, Tag
 
 class PostListView(ListView):
     model = Post
     ordering = ['-timestamp']
+    
+class FilteredPostListView(ListView):
+    model = Post
+    ordering = ['-timestamp']
+
+    def get_queryset(self):
+        return super(FilteredPostListView, self).get_queryset().filter(tags__in=[Tag.objects.get(name=self.kwargs['tag'])])
--- smtd/urls.py
+++ smtd/urls.py
@@ -13,13 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from blog.views import PostListView
+from blog.views import FilteredPostListView, PostListView
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', PostListView.as_view())
+    path('posts/', PostListView.as_view()),
+    re_path(r'^posts/(?P<tag>\w+)/$', FilteredPostListView.as_view())
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
