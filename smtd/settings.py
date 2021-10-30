7741bf30cb6bc08a9fde865d5da1dad17dd2eba6
Pieternella Luttikhuizen <pieternella.luttikhuizen@hva.nl>
2021-10-30 19:28:02 +0200

    set to production

--- smtd/settings.py
+++ smtd/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-)vhr5lbbqoju^5$zbgrt71l2z^yc-dl7l$h76cji2ys^(9&b-@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["showmethedata.pythonanywhere.com", "127.0.0.1"]
 
